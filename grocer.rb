3ecb9834220298975d690bbc39b5923c1de1cfbd
jonathan choi <jisung594@gmail.com>
2018-12-10 01:50:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,23 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  count = Hash.new(0)
+
+  cart.each do |hash|
+    count[hash] += 1
+  end
+
+  new_hash = {}
+
+  count.each do |orig_hash,num|
+    orig_hash.each do |veg,info|
+      new_hash[veg] = info
+      new_hash[veg][info].merge(count: num)
+      binding.pry
+    end
+  end
+
+  return new_hash
 end
 
 def apply_coupons(cart, coupons)
