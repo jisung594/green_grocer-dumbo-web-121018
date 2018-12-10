fef2793bf7ed3fabbbe23899575056acfd57735b
jonathan choi <jisung594@gmail.com>
2018-12-10 03:44:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,46 @@
+require 'pry'
+
+# def consolidate_cart(cart)
+#   count = Hash.new(0)
+#
+#   cart.each do |hash|
+#     count[hash] += 1
+#   end
+#
+#   new_hash = {}
+#
+#   count.each do |orig_hash,num|
+#     orig_hash.each do |veg,info|
+#       new_hash[veg] = info
+#       new_hash[veg][info].merge(count: num)
+#       binding.pry
+#     end
+#   end
+#
+#   return new_hash
+# end
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+
+  cart.each do |hash|
+    hash.each do |veg,info_hash|
+      new_hash[veg] ||= {}
+      new_hash[veg][:count] ||= 0  # if new_hash[veg][:count] is undefined, then assign 0 as the value
+      new_hash[veg][:count] += 1
+        info_hash.each do |attribute,val|
+          new_hash[veg][attribute] = val
+            binding.pry
+        end
+    end
+  end
+
+  return new_hash
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  cart
 end
 
 def apply_clearance(cart)
